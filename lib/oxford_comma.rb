92323ce55d0a38438de5c5fdfd3a510b758b7ee4
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 21:05:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,24 @@
+#def oxford_comma(array)
+#  if array.length == 1
+#    array.join(",")
+#  elsif array.length == 2
+#    array.insert(1, "and")
+#    array.join(" ")
+#  else
+#    last = array.pop
+#    array = array.collect { |x| x + ","}
+#    array << last
+#    and_place = array.length - 1
+#    array.insert(and_place, "and")
+#    array.join(" ")
+#  end
+#end
+
 def oxford_comma(array)
-  if array.length == 1
-    array.join(",")
-  elsif array.length == 2
-    array.insert(1, "and")
-    array.join(" ")
-  else
-    last = array.pop
-    array = array.collect { |x| x + ","}
-    array << last
-    and_place = array.length - 1
-    array.insert(and_place, "and")
-    array.join(" ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")    
 end
